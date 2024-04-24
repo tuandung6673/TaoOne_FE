0ae93fc488904dd89ad63eb3b651f2a58fbc0f48
tuandung6673 <tuandung6673@gmail.com>
2024-04-25 00:55:45 +0700

    update header

--- src/layouts/User/UserLayout.tsx
+++ src/layouts/User/UserLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import UserHeader from '../../components/Header/user-header/UserHeader';
-import Footer from "../../components/footer/Footer";
+// import Footer from "../../components/footer/Footer";
 
 function UserLayout() {
     return (
         <div>
             <UserHeader></UserHeader>
             <Outlet />
-            <Footer/>
+            {/* <Footer/> */}
         </div>
     )
 }

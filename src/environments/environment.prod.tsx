7b0835c806e385a7bab96517879cc3c402a5af70
tuandung6673 <tuandung6673@gmail.com>
2024-12-23 16:12:29 +0700

    update

--- src/environments/environment.prod.tsx
+++ src/environments/environment.prod.tsx
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     baseUrl: 'https://localhost:7079/api',
     tele_bot_token: "7898125233:AAHH1K55m9m9buZXDeUs8fs6jCWBBK3g2Gk",
-    tele_chat_id: "-4715991566"
+    tele_chat_id: "-4715991566",
+    vietnamApi: 'https://provinces.open-api.vn/api'
   };

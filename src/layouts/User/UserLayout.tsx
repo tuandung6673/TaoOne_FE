7e835793e06db3b43a4528595a9b0f797f23d550
tuandung6673 <tuandung6673@gmail.com>
2025-01-15 16:55:25 +0700

    updae

--- src/layouts/User/UserLayout.tsx
+++ src/layouts/User/UserLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import UserHeader from "../../components/Header/user-header/UserHeader";
 import Footer from "../../components/footer/Footer";
+import classes from './UserLayout.module.scss'
 // import Footer from "../../components/footer/Footer";
 
 function UserLayout() {
     return (
         <div>
             <UserHeader></UserHeader>
-            <Outlet />
+            <div className={classes.outlet_container}>
+                <Outlet />
+            </div>
             <Footer></Footer>
         </div>
     );

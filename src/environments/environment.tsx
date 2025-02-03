64ef5d200f590421c7b37689d8c75466ebd59591
tuandung6673 <tuandung6673@gmail.com>
2025-02-03 22:30:00 +0700

    update baseurl

--- src/environments/environment.tsx
+++ src/environments/environment.tsx
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://localhost:7079/api',
+    baseUrl: 'http://103.195.238.48:5000/api',
     tele_bot_token: "7898125233:AAHH1K55m9m9buZXDeUs8fs6jCWBBK3g2Gk",
     tele_chat_id: "-4715991566",
     vietnamApi: 'https://provinces.open-api.vn/api'

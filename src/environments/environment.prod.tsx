b39f80f5f5dbd38d06ff5580686970a4e9d4223d
tuandung6673 <tuandung6673@gmail.com>
2025-02-25 00:18:17 +0700

    update

--- src/environments/environment.prod.tsx
+++ src/environments/environment.prod.tsx
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://103.195.238.48:5000/api',
+    baseUrl: 'http://taoone.vn/api',
     tele_bot_token: "7898125233:AAHH1K55m9m9buZXDeUs8fs6jCWBBK3g2Gk",
     tele_chat_id: "-4715991566",
     vietnamApi: 'https://provinces.open-api.vn/api',

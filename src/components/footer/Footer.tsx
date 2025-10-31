0b7b5659fe04ba3ff2cecda960e122a164194b03
tuandung6673 <tuandung6673@gmail.com>
2025-10-31 10:42:24 +0700

    update dia chi

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
                     Email: taoonestore@gmail.com
                 </div>
                 <div className="footer_item">
-                    Địa chỉ: 377 Giải Phóng - Thanh Xuân - HN
+                    Địa chỉ: Giải Phóng - Thanh Xuân - HN
                 </div>
             </div>
         </div>
